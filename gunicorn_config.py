ed6c8b01e97c448ec389351e62c3aa1f45edbed9
Osvic1 <Timothyv952@gmail.com>
2025-09-19 02:07:32 +0100

    update gunicorn1

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,8 +6,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # Bind to all interfaces on port 8000
 bind = "0.0.0.0:8000"
 
-# Load the Flask app using the factory pattern
-
 
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %d)", worker.pid)
@@ -15,8 +13,3 @@ def post_fork(server, worker):
 
 def when_ready(server):
     server.log.info("Gunicorn is ready and listening at http://%s", bind)
-
-
-def init(server):
-    from backend.app import create_app  # Import the factory function
-    server.app = create_app()          # Call the factory to get the app instance
